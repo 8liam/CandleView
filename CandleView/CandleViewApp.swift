d6ba488a5667b0d25a799a05f7de8dbcca214054
Liam <n11102764@qut.edu.au>
2025-08-19 20:51:21 +1000

    Initial Commit

---
+++ CandleView/CandleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandleViewApp.swift
+//  CandleView
+//
+//  Created by liam on 19/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct CandleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
